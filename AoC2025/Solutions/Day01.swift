417a253781e2330ba66179c04144894cd2a89f33
Roman Mirzoyan <roman.mirzoyan@erstegroup.com>
2025-12-01 20:55:54 +0100

    Day 1

--- AoC2025/Solutions/Day01.swift
+++ AoC2025/Solutions/Day01.swift
@@ -2,10 +2,63 @@ enum Day01: Day {
     static let input = ""
 
     static func part1() -> Int {
-        0
+        input
+            .split(separator: "\n")
+            .map { Int($0.replacing("L", with: "-").replacing("R", with: "+"))! }
+            .reduce((lastPointer: 50, zeroReached: 0)) { partialResult, rotation in
+                let (lastPointer, zeroReached) = partialResult
+
+                var newPointer = lastPointer + rotation
+
+                if newPointer < 0 {
+                    newPointer = 100 * (abs(newPointer) / 100 + 1) + newPointer
+                }
+
+                newPointer %= 100
+
+                return (newPointer, newPointer == 0 ? zeroReached + 1 : zeroReached)
+            }
+            .zeroReached
     }
 
     static func part2() -> Int {
-        0
+        input
+            .split(separator: "\n")
+            .map { Int($0.replacing("L", with: "-").replacing("R", with: "+"))! }
+            .reduce((lastPointer: 50, zeroClicks: 0)) { partialResult, rotation in
+                let lastPointer = partialResult.lastPointer
+
+                var newPointer = lastPointer + rotation
+
+                var zeroClicks = partialResult.zeroClicks
+
+                switch newPointer {
+                case 1...99:
+                    break
+                case 0:
+                    zeroClicks += 1
+                case 100...:
+                    zeroClicks += newPointer / 100
+                case ..<0:
+                    let absPointer = abs(newPointer)
+
+                    if lastPointer == 0 {
+                        if absPointer >= 100 {
+                            zeroClicks += absPointer / 100
+                        }
+                    } else {
+                        zeroClicks += absPointer / 100 + 1
+                    }
+                default:
+                    break
+                }
+
+                if newPointer < 0 {
+                    newPointer = 100 * (abs(newPointer) / 100 + 1) + newPointer
+                }
+
+                return (newPointer % 100, zeroClicks)
+            }
+            .zeroClicks
     }
 }
